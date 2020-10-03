d0bbd91b6af47640c1e26daf3f65795ce1553f75
Victoria Lerda <70362533+vlerda@users.noreply.github.com>
2020-10-02 23:30:19 -0700

    Add files via upload

---
+++ compare.py
@@ -0,0 +1,11 @@
+import math
+from numpy import isclose
+a=3.
+if math.sqrt(a)**2 != a:
+    print("WTH??")
+tol=1e-10
+if abs(math.sqrt(a)**2-a) > tol:
+    print("WTH again!?")
+
+if isclose(math.sqrt(a)**2,a):
+    print("Close enough!")
